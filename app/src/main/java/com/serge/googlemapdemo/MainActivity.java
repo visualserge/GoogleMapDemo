98b6e05a275f2ae2d38639e16eaa0ae632ec2dc8
visualserge <sergio.delossantosjr@hotmail.com>
2015-03-29 16:31:33 +0800

    First Commit

--- app/src/main/java/com/serge/googlemapdemo/MainActivity.java
+++ app/src/main/java/com/serge/googlemapdemo/MainActivity.java
@@ -4,15 +4,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.location.*;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
+
+    private double longitude;
+    private double latitude;
 
     //First google map project
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+        LocationManager lm = (LocationManager)getSystemService(getApplicationContext().LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
     }
 
 
@@ -36,5 +52,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onMapReady(GoogleMap map) {
+        LatLng myLocation = new LatLng(latitude, longitude);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("My current location")
+                .snippet("Hi Sergio! :)")
+                .position(myLocation));
     }
 }
