927b600dfb845670e06f413a4a2956975831e704
visualserge <sergio.delossantosjr@hotmail.com>
2015-03-29 16:04:03 +0800

    First Commit

--- app/src/main/java/com/serge/googlemapdemo/MainActivity.java
+++ app/src/main/java/com/serge/googlemapdemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    //First google map project
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
